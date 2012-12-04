44eadb34bcaa54f7097b0a3cf706f0bb9d538cb0
Tarek Ziade <tarek@ziade.org>
2012-12-04 10:43:20 +0100

    multiple hosts

--- vaurienclient/ext/_marteau.py
+++ vaurienclient/ext/_marteau.py
@@ -8,12 +8,13 @@ class VaurienFixture(object):
     """Setup a vaurien proxy in a particular state, and restore the original
        state at the end of the tests.
     """
-    def __init__(self, behavior='dummy', host='http://localhost:80',
+    def __init__(self, behavior='dummy', hosts='http://localhost:80',
                  **kwargs):
         self._old_behavior = None
         self.host = host
         self.behavior = behavior
-        self._client = self._get_client(host)
+        hosts = hosts.split(',')
+        self._clients = [self._get_client(host) for host in hosts]
         self._proxy_args = kwargs
 
     def _get_client(self, host):
@@ -37,12 +38,14 @@ class VaurienFixture(object):
         # first of all, make a call to the proxy to get its current state, in
         # order to replace everythin at the right position in the tearDown
         # method.
-        self._old_behavior = self._client.get_behavior()
-        self.set_behavior(self.behavior, **self._proxy_args)
+        for client in self._clients:
+            self._old_behavior = self._client.get_behavior()
+            self.set_behavior(self.behavior, **self._proxy_args)
 
     def tear_down(self):
         """Restore back the previous behavior on the proxy"""
-        self._client.set_behavior(self._old_behavior)
+        for client in self._clients:
+            self._client.set_behavior(self._old_behavior)
 
 
 MarteauFixture.register(VaurienFixture)
