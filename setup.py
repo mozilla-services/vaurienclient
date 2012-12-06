822cf66535b9c827b11ea245d7a6d1d4a23095c1
Tarek Ziade <tarek@ziade.org>
2012-12-06 13:25:05 +0100

    prep for releas

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = ["Programming Language :: Python",
 
 
 setup(name='vaurienclient',
-      version='0.8',
+      version='0.9',
       packages=find_packages(),
       description=("Client for the TCP Chaos Proxy"),
       long_description=README,
